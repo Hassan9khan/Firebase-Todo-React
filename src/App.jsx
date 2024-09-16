1910b876f310831353950c2fc0b1e88b9ef4fe75
Hassan Ahmed <hassanshakeel381@gmail.com>
2024-09-16 22:39:28 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-screen w-screen text-center text-4xl bg-gradient-to-r from-[#2670d2] to-[#7bc0d3]"></div>
+  );
+};
+
+export default App;
